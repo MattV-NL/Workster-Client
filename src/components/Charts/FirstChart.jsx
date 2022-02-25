596fbdb7384ef6f3c5592c5938bf4302d7db92c4
mgorgeov97 <matthewggvey87@hotmail.com>
2022-02-24 21:22:05 -0330

    working with c3 reference website and chart formating

--- src/components/Charts/FirstChart.jsx
+++ src/components/Charts/FirstChart.jsx
@@ -12,31 +12,26 @@ const FirstChart = () => {
 
       bindto: '#chart',
       data: {
-        columns: [
-          ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 50, 20, 10, 40, 15, 25]
-        ],
-        axes: {
-          data2: 'y2'
-        },
-        types: {
-          data2: 'bar'
-        }
+        data: {
+          json: [
+              {name: 'Monday', Precipitation: 20, WindSpeed: 65},
+              {name: 'Tuesday', Precipitation: 10, WindSpeed: 50},
+              {name: 'Wednesday', Precipitation: 0, WindSpeed: 25},
+              {name: 'Thursday', Precipitation: 40, WindSpeed: 85},
+              {name: 'Friday', Precipitation: 0, WindSpeed: 55},
+              {name: 'Saturday', Precipitation: 0, WindSpeed: 35},
+              {name: 'Sunday', Precipitation: 15, WindSpeed: 60},
+          ],
+          keys: {
+              x: 'name',
+              value: ['Precipitation', 'WindSpeed'],
+          }
       },
       axis: {
-        y: {
-          label: {
-            text: 'Y Label',
-            position: 'outer-middle'
+          x: {
+              type: 'category'
           }
-        },
-        y2: {
-          show: true,
-          label: {
-            text: 'Y2 Label',
-            position: 'outer-middle'
-          }
-        }
+      }
       }
     });
   }, []);
