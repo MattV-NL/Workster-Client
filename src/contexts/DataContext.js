3b4f164dc79ba63b55c0cee95488d5b01be81b81
mgorgeov97 <matthewggvey87@hotmail.com>
2022-03-26 14:36:03 -0230

    working with luxon to remove repeating code from before

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -5,23 +5,14 @@ export const DataContext = createContext();
 
 const DataContextProvider = (props) => {
 
-  const date = DateTime.now().toFormat('yyyy-MM-dd');
-  const date1 = DateTime.now().plus({days: 1}).toFormat('yyyy-MM-dd');
-  const date2 = DateTime.now().plus({days: 2}).toFormat('yyyy-MM-dd');
-  const date3 = DateTime.now().plus({days: 3}).toFormat('yyyy-MM-dd');
-  const date4 = DateTime.now().plus({days: 4}).toFormat('yyyy-MM-dd');
-  const date5 = DateTime.now().plus({days: 5}).toFormat('yyyy-MM-dd');
-  const date6 = DateTime.now().plus({days: 6}).toFormat('yyyy-MM-dd');
+  const date = DateTime.local(); 
 
-  const [weatherValues, setWeatherValues] = useState([
-    {date: date, Precipitation: 0, WindSpeed: 0},
-    {date: date1, Precipitation: 0, WindSpeed: 0},
-    {date: date2, Precipitation: 0, WindSpeed: 0},
-    {date: date3, Precipitation: 0, WindSpeed: 0},
-    {date: date4, Precipitation: 0, WindSpeed: 0},
-    {date: date5, Precipitation: 0, WindSpeed: 0},
-    {date: date6, Precipitation: 0, WindSpeed: 0}
-  ]);
+  const [weatherValues, setWeatherValues] = useState(
+    Array(7).fill(date).map((date, index) => {
+      var nextDate = date.plus({days: index})
+      return nextDate;
+    }).map((date) => ({date: date.toFormat('yyyy-MM-dd'), Precipitation: 0, WindSpeed: 0}))
+  );
 
   const submitWeatherValues = (weekday, precip, wind) => {
     setWeatherValues((prevWeatherValues) =>
