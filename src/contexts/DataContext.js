2aa4e8ad78d1252bb85b9feb2386e1b2813d1ef4
mgorgeov97 <matthewggvey87@hotmail.com>
2022-04-02 22:06:14 -0230

    working with Map() to condense code, still working on funcitonality

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -3,21 +3,31 @@ import { DateTime } from 'luxon';
 
 export const DataContext = createContext();
 
+const date = DateTime.local(); 
+
+  const weatherArray = Array(7).fill(date).map((date, index) => {
+    var nextDate = date.plus({ days: index })
+    return nextDate;
+    }).map((date) => ({date: date.toFormat('yyyy-MM-dd'), precip: 0, wind: 0}));
+
+  const weatherMap = new Map();
+  
+  weatherArray.map(({ date, precip, wind }) => {
+    weatherMap.set(({ date }), ({ date, precip, wind }));
+  })
+  console.log(weatherMap)
+
+
 const DataContextProvider = ({ children }) => {
 
-  const date = DateTime.local(); 
-
-  const [weatherValues, setWeatherValues] = useState(
-    Array(7).fill(date).map((date, index) => {
-      var nextDate = date.plus({ days: index })
-      return nextDate;
-    }).map((date) => ({date: date.toFormat('yyyy-MM-dd'), precip: 0, wind: 0}))
-  );
+  const [weatherValues, setWeatherValues] = useState(weatherArray);
 
   const submitWeatherValues = (date, precip, wind) => {
-    setWeatherValues([...weatherValues, {date, precip, wind}]
-      );
-  }
+    weatherMap.set(({ date }), ({ date, precip, wind }));
+
+    console.log(weatherMap);
+    setWeatherValues(Array.from(weatherMap.values()));
+    }
 
   return (
     <DataContext.Provider value={{ weatherValues, submitWeatherValues }}>
