771051390eed59526dc31a7ef8757ed27c906ac5
mgorgeov97 <matthewggvey87@hotmail.com>
2022-04-24 19:33:07 -0230

    created an inital entry for the table and added some styling if the work details overflows

--- src/contexts/WorkDataContext.js
+++ src/contexts/WorkDataContext.js
@@ -1,8 +1,29 @@
 import { createContext, useState } from 'react';
+import { DateTime } from 'luxon';
 
 export const WorkDataContext = createContext();
 
+const createFormDate = () => {
+  return new DateTime.utc();
+};
+
+const workDataArray = Array(1)
+  .fill(createFormDate())
+  .map((item) => ({ formDate: item.toISODate() }));
+
 const workDataMap = new Map();
+
+workDataArray.map(
+  ({ formDate, isOutside, isWelding, isScaffolding, workDetails }) => {
+    workDataMap.set(parseInt(formDate.replace(/-/g, '')), {
+      formDate,
+      isOutside,
+      isWelding,
+      isScaffolding,
+      workDetails,
+    });
+  }
+);
 
 const WorkDataContextProvider = ({ children }) => {
   const [workValues, setWorkValues] = useState(
@@ -16,14 +37,16 @@ const WorkDataContextProvider = ({ children }) => {
     isScaffolding,
     workDetails
   ) => {
-    workDataMap.set(formDate, {
+    workDataMap.set(parseInt(formDate.replace(/-/g, '')), {
       formDate,
       isOutside,
       isWelding,
       isScaffolding,
       workDetails,
     });
-    const nextWorkDataMap = new Map([...workDataMap]);
+    const nextWorkDataMap = new Map(
+      [...workDataMap].sort((a, b) => a[0] - b[0])
+    );
     setWorkValues(Array.from(nextWorkDataMap.values()));
   };
 
