a2deff6566183e60ab3615bd658bac550b431ee0
mgorgeov97 <matthewggvey87@hotmail.com>
2022-05-16 22:28:17 -0230

    changes made during meeting, going to continue to addresss styling concerns with white text at a later date due to time of our meeting

--- src/components/Charts/WorkChart.jsx
+++ src/components/Charts/WorkChart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useCallback } from 'react';
+import { useContext, useEffect, useCallback, useMemo } from 'react';
 import { WorkDataContext } from '../../contexts/WorkDataContext';
 import './c3.min.css';
 import c3 from 'c3';
@@ -7,79 +7,61 @@ import './charts.scss';
 const WorkChart = () => {
   const { workValues } = useContext(WorkDataContext);
 
-  let outCounter = 0;
-  let weldCounter = 0;
-  let scaffCounter = 0;
+  const json = useMemo(() => {
+    const arrWork = [...workValues.values()];
 
-  Array.from(workValues.values()).map(
-    ({ isOutside, isWelding, isScaffolding }) => {
-      if (isOutside) {
-        outCounter++;
-      }
-      if (isWelding) {
-        weldCounter++;
-      }
-      if (isScaffolding) {
-        scaffCounter++;
-      }
-    }
-  );
-
-  const workDataArr = [
-    { id: 'isOutside', value: outCounter },
-    { id: 'isWelding', value: weldCounter },
-    { id: 'isScaffolding', value: scaffCounter },
-  ];
-
-  const values = useCallback(
-    () => workDataArr.map((item) => item.value),
-    [workDataArr]
-  );
+    return [
+      {
+        id: 'isOutside',
+        isOutside: arrWork.filter(({ isOutside }) => isOutside).length,
+      },
+      {
+        id: 'isWelding',
+        isWelding: arrWork.filter(({ isWelding }) => isWelding).length,
+      },
+      {
+        id: 'isScaffolding',
+        isScaffolding: arrWork.filter(({ isScaffolding }) => isScaffolding)
+          .length,
+      },
+    ];
+  }, [workValues]);
 
   useEffect(() => {
     c3.generate({
       bindto: '#chart',
       data: {
-        json: workDataArr,
+        json,
         keys: {
           x: 'id',
-          value: ['value'],
+          value: ['isOutside', 'isWelding', 'isScaffolding'],
         },
+        groups: [['isOutside', 'isWelding', 'isScaffolding']],
         colors: {
           isOutside: '#0000ff',
           isWelding: '#4444d4',
           isScaffolding: '#5c5cfa',
         },
         type: 'bar',
-        names: {
-          isOutside: 'Outside',
-          isWelding: 'Welding',
-          isScaffolding: 'Scaffolding',
-        },
       },
       bar: {
         width: {
-          ratio: 0.8,
+          ratio: 0.6,
         },
       },
       axis: {
         x: {
           type: 'category',
-          categories: ['', '', ''],
         },
         y: {
-          label: {
-            text: 'Number of days for each task in the work scope',
-            position: 'outer-middle',
-          },
           tick: {
-            values: values,
+            format: (num) => (parseInt(num) === num ? num : undefined),
           },
         },
       },
     });
-  }, [workDataArr]);
-  return <div id='chart' className='work-chart'></div>;
+  }, [json]);
+  return <div id='chart'></div>;
 };
 
 export default WorkChart;
