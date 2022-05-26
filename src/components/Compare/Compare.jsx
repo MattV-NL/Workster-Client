e9df817fed15c84dc8021fbab501f0c775b5128a
mgorgeov97 <matthewggvey87@hotmail.com>
2022-05-26 21:22:58 -0230

    working on function to compare the values, not functional yet

--- src/components/Compare/Compare.jsx
+++ src/components/Compare/Compare.jsx
@@ -1,13 +1,11 @@
 import { useContext } from 'react';
 import { WeatherDataContext } from '../../contexts/WeatherDataContext';
 import { WorkDataContext } from '../../contexts/WorkDataContext';
+import Button from '../Inputs/Button';
 
 const Compare = () => {
   const { weatherValues } = useContext(WeatherDataContext);
   const { workValues } = useContext(WorkDataContext);
-
-  console.log(weatherValues);
-  console.log(workValues);
 
   /*
   Compare the maps to see if there is significant weather on days
@@ -23,11 +21,34 @@ const Compare = () => {
   if they are not below a threshold return a seperate message
 
   could also display both charts on this page to add content
-
-  have a button to initiate the comparison
   */
 
-  return <div>hello</div>;
+  const compareValues = () => {
+    Array.from(weatherValues.values()).map(({ precip, wind }) => {
+      Array.from(workValues.values()).map(
+        ({ isOutside, isWelding, isScaffolding }) => {
+          if (
+            (precip > 20 || wind > 40) &&
+            (isOutside || isWelding || isScaffolding)
+          ) {
+            console.log(
+              'perhaps reconsider you work schedule, there is weather on days where you have work planned that is not suited to deal with weather'
+            );
+          } else {
+            console.log(
+              'Great! Your schedule has no conflicts with the weather'
+            );
+          }
+        }
+      );
+    });
+  };
+
+  return (
+    <div>
+      <Button onClick={compareValues}>Compare Work and Weather</Button>
+    </div>
+  );
 };
 
 export default Compare;
