9676f1e50ac2233b2499d7a58ed9ce3283f87b6a
mgorgeov97 <matthewggvey87@hotmail.com>
2022-06-27 20:37:40 -0230

    this branch will be used for me to implement a weather API instead of relying on the user to input their own information

---
+++ src/contexts/WeatherDataContext2.js
@@ -0,0 +1,9 @@
+import { useContext, createContext, useState } from 'react';
+
+export const WeatherDataContext2 = createContext();
+
+const WeatherDataContext2 = () => {
+  return <div></div>;
+};
+
+export default WeatherDataContext2;
