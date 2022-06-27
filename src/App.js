51f9483ca5154e176f0f65e2c0c78ea31ff8cc49
mgorgeov97 <matthewggvey87@hotmail.com>
2022-06-27 21:22:36 -0230

    retrieved weather information from API, working on getting the information usable for c3 and the tables I ahve in place

--- src/App.js
+++ src/App.js
@@ -20,53 +20,56 @@ import ResetWarningModal from './components/Modals/ResetWarningModal';
 import WarningModal from './components/Modals/WarningModal';
 import SortWeatherTable from './components/SortTable/SortWeatherTable';
 import SortWorkTable from './components/SortTable/SortWorkTable';
+import WeatherDataContextProvider2 from './contexts/WeatherDataContext2';
 
 function App() {
   return (
-    <WeatherDataContextProvider>
-      <WeatherInputContextProvider>
-        <WorkDataContextProvider>
-          <WorkInputContextProvider>
-            <Router>
-              <Header />
-              <div className='layout-center'>
-                <div className='layout'>
-                  <Switch>
-                    <Route exact path={['/', paths.HOME]}>
-                      <Home />
-                    </Route>
-                    <Route exact path={paths.WEATHER}>
-                      <SortWeatherTable />
-                      <WeatherTable />
-                      <WeatherButton />
-                      <WeatherChart />
-                      <WeatherResetButton />
-                      <WarningModal />
-                      <ResetWarningModal />
-                    </Route>
-                    <Route exact path={paths.WORK}>
-                      <WorkForm />
-                      <SortWorkTable />
-                      <WorkTable />
-                      <WorkChart />
-                      <WorkResetButton />
-                      <WarningModal />
-                      <ResetWarningModal />
-                    </Route>
-                    <Route exact path='/work-details/:id'>
-                      <WorkDetails />
-                    </Route>
-                    <Route>
-                      <NotFound />
-                    </Route>
-                  </Switch>
+    <WeatherDataContextProvider2>
+      <WeatherDataContextProvider>
+        <WeatherInputContextProvider>
+          <WorkDataContextProvider>
+            <WorkInputContextProvider>
+              <Router>
+                <Header />
+                <div className='layout-center'>
+                  <div className='layout'>
+                    <Switch>
+                      <Route exact path={['/', paths.HOME]}>
+                        <Home />
+                      </Route>
+                      <Route exact path={paths.WEATHER}>
+                        <SortWeatherTable />
+                        <WeatherTable />
+                        <WeatherButton />
+                        <WeatherChart />
+                        <WeatherResetButton />
+                        <WarningModal />
+                        <ResetWarningModal />
+                      </Route>
+                      <Route exact path={paths.WORK}>
+                        <WorkForm />
+                        <SortWorkTable />
+                        <WorkTable />
+                        <WorkChart />
+                        <WorkResetButton />
+                        <WarningModal />
+                        <ResetWarningModal />
+                      </Route>
+                      <Route exact path='/work-details/:id'>
+                        <WorkDetails />
+                      </Route>
+                      <Route>
+                        <NotFound />
+                      </Route>
+                    </Switch>
+                  </div>
                 </div>
-              </div>
-            </Router>
-          </WorkInputContextProvider>
-        </WorkDataContextProvider>
-      </WeatherInputContextProvider>
-    </WeatherDataContextProvider>
+              </Router>
+            </WorkInputContextProvider>
+          </WorkDataContextProvider>
+        </WeatherInputContextProvider>
+      </WeatherDataContextProvider>
+    </WeatherDataContextProvider2>
   );
 }
 
--- src/contexts/WeatherDataContext2.js
+++ src/contexts/WeatherDataContext2.js
@@ -1,9 +1,39 @@
-import { useContext, createContext, useState } from 'react';
+import { createContext } from 'react';
 
 export const WeatherDataContext2 = createContext();
 
-const WeatherDataContext2 = () => {
-  return <div></div>;
+const fetchWeather = () => {
+  let lon = -52.7314;
+  let lat = 47.6666;
+  let key = 'c4aa91c492141719621c2f09ce2559a3';
+  let lang = 'en';
+  let units = 'metric';
+  let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+
+  fetch(url)
+    .then((resp) => {
+      if (!resp.ok) throw new Error(resp.statusText);
+      return resp.json();
+    })
+    .then((data) => {
+      showWeather(data);
+    })
+    .catch(console.err);
 };
 
-export default WeatherDataContext2;
+const showWeather = (data) => {
+  console.log(data);
+};
+
+fetchWeather();
+
+const something = '';
+const WeatherDataContextProvider2 = ({ children }) => {
+  return (
+    <WeatherDataContext2.Provider value={something}>
+      {children}
+    </WeatherDataContext2.Provider>
+  );
+};
+
+export default WeatherDataContextProvider2;
