3da5338a770b08af27c7dd296ac1a16ff7fece4b
mgorgeov97 <matthewggvey87@hotmail.com>
2022-07-22 13:55:46 -0230

    updated code to use information from API provided by the server

--- src/components/WeatherInput/FetchButton.jsx
+++ src/components/WeatherInput/FetchButton.jsx
@@ -1,55 +1,16 @@
 import { useContext, useCallback } from 'react';
 import { WeatherDataContext } from '../../contexts/WeatherDataContext';
-import { SERVER_URL } from '../../constants';
 import Button from '../Inputs/Button';
 
 const FetchButton = () => {
-  const { fetchWeather, setWeather, setupChart } =
-    useContext(WeatherDataContext);
+  const { getLocation } = useContext(WeatherDataContext);
 
-  // working on getting position to server
-  let lat, lon;
-
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
-  const error = (err) => {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  };
-
-  const success = async (pos) => {
-    const crd = pos.coords;
-    lat = crd.latitude;
-    lon = crd.longitude;
-    console.log(lat, lon);
-
-    const apiUrl = `${SERVER_URL.weather}${lat},${lon}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
-  };
-
-  const getLocation = useCallback(() => {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  }, [success]);
-
-  const handleClick2 = () => {
+  const handleClick = useCallback(() => {
     getLocation();
-  };
-  // end of new
-
-  const handleClick = () => {
-    // setWeather();
-    // setupChart();
-    fetchWeather().catch((err) => console.log(err));
-  };
+  }, [getLocation]);
 
   return (
     <div>
-      <Button onClick={handleClick2}>Send Position</Button>
       <Button onClick={handleClick}>Get Weather Info</Button>
     </div>
   );
--- src/contexts/WeatherDataContext.js
+++ src/contexts/WeatherDataContext.js
@@ -3,42 +3,75 @@ import { DateTime } from 'luxon';
 import { SERVER_URL } from '../constants';
 
 export const WeatherDataContext = createContext();
-// working on table functionality with server side code
-let weatherData = Array(7).fill({ dt: 0, pop: 0, wind_speed: 0 });
-let weatherDataMap = new Map(weatherData.map((data) => [data.dt, data]));
-// end of new
+let lat, lon, weatherDataMap;
 
-const fetchWeather = async () => {
-  const response = await fetch(SERVER_URL.weather);
-  const data = await response.json();
-  console.log(data);
-  storeWeatherData(data);
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
 };
 
-const createDate = (time) =>
+const error = (err) => {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+const createDate = () => {
+  return new DateTime.utc();
+};
+
+const createWeatherValues = () =>
+  new Map(
+    Array(7)
+      .fill(createDate())
+      .map((date, days) => date.plus({ days }))
+      .map((date) => ({
+        date: date.toISODate(),
+        precip: 0,
+        wind: 0,
+      }))
+      .map((data) => [parseInt(data.date.replace(/-/g, '')), data])
+  );
+
+const replaceDate = (time) =>
   parseInt(new DateTime.fromMillis(time).toISODate().replace(/-/g, ''));
 
 const storeWeatherData = ({ daily }) => {
   weatherDataMap = new Map(
-    daily.map((day) => [createDate(day.dt * 1000), day])
+    daily.map((day) => [replaceDate(day.dt * 1000), day])
   );
 };
 
-// fetchWeather().catch((err) => console.log(err));
-
 const WeatherDataContextProvider = ({ children }) => {
   const [weatherValues, setWeatherValues] = useState();
-  const [weatherChartValues, setWeatherChartValues] = useState(
-    Array(7).fill({})
-  );
+  const [weatherChartValues, setWeatherChartValues] =
+    useState(createWeatherValues);
+
+  const success = async (pos) => {
+    const crd = pos.coords;
+    lat = crd.latitude;
+    lon = crd.longitude;
+    console.log(lat, lon);
+
+    const apiUrl = `${SERVER_URL.weather}${lat},${lon}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    storeWeatherData(data);
+    setWeather();
+    setupChart();
+  };
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  };
 
   const setWeather = useCallback(() => {
     setWeatherValues(weatherDataMap);
   }, [setWeatherValues]);
 
   const setupChart = useCallback(() => {
-    setWeatherChartValues(
-      Array.from(weatherDataMap.values()).map(({ dt, pop, wind_speed }) => {
+    const nextWeatherArray = Array.from(weatherDataMap.values()).map(
+      ({ dt, pop, wind_speed }) => {
         let date = new DateTime.fromMillis(dt * 1000).toISODate();
         let precip = pop * 100;
         let wind = wind_speed * 3.6;
@@ -47,23 +80,24 @@ const WeatherDataContextProvider = ({ children }) => {
           precip: parseFloat(precip.toFixed(2)),
           wind: parseFloat(wind.toFixed(2)),
         };
-      })
+      }
     );
-  }, [setWeatherChartValues]);
 
-  const weatherChartMap = new Map(
-    weatherChartValues.map((data) => [data.date, data])
-  );
+    const nextWeatherMap = new Map(
+      nextWeatherArray.map((data) => [
+        parseInt(data.date.replace(/-/g, '')),
+        data,
+      ])
+    );
+    setWeatherChartValues(nextWeatherMap);
+  }, [setWeatherChartValues]);
 
   return (
     <WeatherDataContext.Provider
       value={{
-        fetchWeather,
+        getLocation,
         weatherValues,
-        setWeather,
         weatherChartValues,
-        weatherChartMap,
-        setupChart,
       }}
     >
       {children}
