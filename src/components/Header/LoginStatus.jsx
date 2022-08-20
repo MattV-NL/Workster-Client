96916f70af16af3cd6b250ae576094b14eb08cab
Matt Vey <matthewvey@outlook.com>
2022-08-19 21:49:32 -0230

    working with communicating with backend with JWTs

--- src/components/Header/LoginStatus.jsx
+++ src/components/Header/LoginStatus.jsx
@@ -1,12 +1,28 @@
-import { useContext } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { AuthenticationContext } from '../../contexts/AuthenticationContext';
 
 const LoginStatus = () => {
+  const [authStatus, setAuthStatus] = useState(false);
   const { loginStatus } = useContext(AuthenticationContext);
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      const response = await fetch('http://localhost:8000/authCheck', {
+        headers: {
+          'x-access-token': localStorage.getItem('token'),
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+      setAuthStatus(data);
+    };
+    checkAuth();
+  }, [setAuthStatus, loginStatus]);
+
   return (
-    <div className={loginStatus ? 'logged-in' : 'not-logged-in'}>
-      {loginStatus === false && 'Not'} Logged In
+    <div className={authStatus.auth ? 'logged-in' : 'not-logged-in'}>
+      {authStatus.auth ? authStatus.username : 'Account '}
+      {authStatus.auth === false && 'Not'} Logged In
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,13 @@
-import { useState } from 'react';
-import { useCallback } from 'react';
-import ButtonComp from '../Inputs/Button';
 import './home.scss';
 import LogInForm from './LogInForm';
 
 const Home = () => {
-  const [authStatus, setAuthStatus] = useState(false);
-  const checkAuth = useCallback(async () => {
-    const response = await fetch('http://localhost:8000/authCheck', {
-      headers: {
-        'x-access-token': localStorage.getItem('token'),
-      },
-    });
-    const data = await response.json();
-    setAuthStatus(data.auth);
-  }, [setAuthStatus]);
   return (
     <div className='home-container'>
       <div className='welcome-message'>
         Please Start by Registering or Signing In
       </div>
       <LogInForm />
-      <ButtonComp type='primary' onClick={checkAuth}>
-        Check Authorization Status
-      </ButtonComp>
-      <div className={authStatus ? 'auth-valid' : 'auth-failed'}>
-        {authStatus ? 'User Authorized' : 'Authorization Failed'}
-      </div>
     </div>
   );
 };
