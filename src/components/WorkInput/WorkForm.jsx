b05aa4ab41127391aa9033c87476e75902b2ad7b
Matt Vey <matthewvey@outlook.com>
2022-10-11 21:47:37 -0230

    minor changes

--- src/components/WorkInput/WorkForm.jsx
+++ src/components/WorkInput/WorkForm.jsx
@@ -16,48 +16,54 @@ const WorkForm = () => {
 
   return (
     <div className={darkMode ? 'dark-form-layout' : 'light-form-layout'}>
-      <form className='form'>
-        {[...workFormInputs.values()].map(({ label, id, type, required }) => {
-          if (type === 'checkbox') {
-            return (
-              <div className='label-input-container' key={id}>
-                <label
-                  className={darkMode ? 'dark-form-label' : 'light-form-label'}
-                >
-                  {label}
-                </label>
-                <Switch
-                  className={darkMode ? 'dark-switch' : 'light-switch'}
-                  defaultChecked={false}
-                  id={id}
-                  onClick={workData[id].onChange}
-                  checked={workData[id].value}
-                  required={required}
-                />
-              </div>
-            );
-          } else {
-            return (
-              <div className='label-input-container' key={id}>
-                <label
-                  className={darkMode ? 'dark-form-label' : 'light-form-label'}
-                >
-                  {label}
-                </label>
-                <InputComp
-                  id={id}
-                  type={type}
-                  value={workData[id].value}
-                  required={required}
-                  onChange={workData[id].onChange}
-                />
-              </div>
-            );
-          }
-        })}
-        {authStatus.auth ? <WorkLocationsDropdown /> : ''}
-      </form>
-      <WorkButton />
+      <div className='form'>
+        <form className='form'>
+          {[...workFormInputs.values()].map(({ label, id, type, required }) => {
+            if (type === 'checkbox') {
+              return (
+                <div className='label-input-container' key={id}>
+                  <label
+                    className={
+                      darkMode ? 'dark-form-label' : 'light-form-label'
+                    }
+                  >
+                    {label}
+                  </label>
+                  <Switch
+                    className={darkMode ? 'dark-switch' : 'light-switch'}
+                    defaultChecked={false}
+                    id={id}
+                    onClick={workData[id].onChange}
+                    checked={workData[id].value}
+                    required={required}
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div className='label-input-container' key={id}>
+                  <label
+                    className={
+                      darkMode ? 'dark-form-label' : 'light-form-label'
+                    }
+                  >
+                    {label}
+                  </label>
+                  <InputComp
+                    id={id}
+                    type={type}
+                    value={workData[id].value}
+                    required={required}
+                    onChange={workData[id].onChange}
+                  />
+                </div>
+              );
+            }
+          })}
+          {authStatus.auth ? <WorkLocationsDropdown /> : ''}
+        </form>
+        <WorkButton />
+      </div>
     </div>
   );
 };
