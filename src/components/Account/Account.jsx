cedabcd34f3253af145293321db978b0220ed2d8
Matt Vey <matthewvey@outlook.com>
2022-11-12 17:50:30 -0330

    more styling and cleaning up components

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import { paths, locationsTableColumns } from '../../constants';
 import { AuthenticationContext } from '../../contexts/AuthenticationContext';
+import '../DashBoard/dashboard.scss';
 import './account.scss';
 import { Table } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
@@ -10,8 +11,8 @@ import DeleteLocationModal from '../Modals/DeleteLocationModal';
 import { getLocations } from '../../restAPI/getAccountLocations';
 import AccountSettingsLink from './AccountSettingsLink';
 import Logout from './Logout';
-import ButtonComp from '../Inputs/Button';
 import { UserSettingsContext } from '../../contexts/UserSettingsContext';
+import { Button } from 'antd';
 
 const Account = () => {
   const { darkMode } = useContext(UserSettingsContext);
@@ -65,10 +66,10 @@ const Account = () => {
   datasource.map((item, index) => (item.key = index));
 
   return (
-    <div className={darkMode ? 'dark-page-layout' : 'light-page-layout'}>
-      <div className='page'>
-        {authStatus.auth ? (
-          <>
+    <div className='account-container'>
+      {authStatus.auth ? (
+        <>
+          <div className={darkMode ? 'dark-page-layout' : 'light-page-layout'}>
             <div
               className={darkMode ? 'dark-page-header' : 'light-page-header'}
             >
@@ -88,13 +89,16 @@ const Account = () => {
             >
               <Table dataSource={datasource} columns={locationsTableColumns} />
             </div>
-          </>
-        ) : (
-          <Link to={paths.DASHBOARD}>
-            <ButtonComp type={'primary'}>to DashBoard</ButtonComp>
-          </Link>
-        )}
-      </div>
+          </div>
+        </>
+      ) : (
+        <Button
+          className={darkMode ? 'dark-dashboard-item' : 'light-dashboard-item'}
+          type='primary'
+        >
+          <Link to={paths.DASHBOARD}>Back to DashBoard</Link>
+        </Button>
+      )}
     </div>
   );
 };
--- src/components/DashBoard/WorkRedirect.jsx
+++ src/components/DashBoard/WorkRedirect.jsx
@@ -1,10 +1,18 @@
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
 import { paths } from '../../constants';
+import { UserSettingsContext } from '../../contexts/UserSettingsContext';
+import { useContext } from 'react';
 
 const WorkRedirect = () => {
+  const { darkMode } = useContext(UserSettingsContext);
+
   return (
-    <Button className='dashboard-item' type='primary' block='true'>
+    <Button
+      className={darkMode ? 'dark-dashboard-item' : 'light-dashboard-item'}
+      type='primary'
+      block='true'
+    >
       <Link to={paths.WORK}>Work</Link>
     </Button>
   );
