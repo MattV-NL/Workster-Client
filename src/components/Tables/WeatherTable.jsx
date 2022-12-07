85e7fb8a16697c1b6938c0bf6dfdd0a75dc3ab7a
Matt Vey <matthewvey@outlook.com>
2022-12-06 20:47:01 -0330

    continuing work on conflict revision

--- src/components/Tables/WeatherTable.jsx
+++ src/components/Tables/WeatherTable.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { WeatherDataContext } from '../../contexts/WeatherDataContext';
 import './tables.scss';
 import { Table } from 'antd';
@@ -18,46 +18,56 @@ const WeatherTable = () => {
   const { isConflict2 } = useContext(ConflictContext);
   const weatherValuesKeys = weatherValues.keys();
   const [weatherDetailsKey, setWeatherDetailsKey] = useState('');
+  const [datasource, setDatasource] = useState([]);
+  const [rowState, setRowState] = useState(false);
 
-  const datasource = Array.from(weatherValues.values()).map(
-    ({ dt, wind_speed, rain, snow }, index) => {
-      const detailsKey = weatherValuesKeys.next().value;
-      const date = new Date(dt * 1000).toDateString();
-      let windSpeed = wind_speed;
-      let speedUnit = 'km/hr';
-      if (units === 'metric' || units === 'standard') {
-        windSpeed = wind_speed * 3.6;
-      } else if (units === 'imperial') {
-        speedUnit = 'mi/hr';
-      }
-      const details = (
-        <>
-          <div
-            className='more-details'
-            onClick={() => {
-              setWeatherDetailsKey(detailsKey);
-              setIsWeatherDetailsVisible(true);
-            }}
-          >
-            More Details...
-          </div>
-        </>
-      );
+  useEffect(() => {
+    setDatasource(
+      Array.from(weatherValues.values()).map(
+        ({ dt, wind_speed, rain, snow }, index) => {
+          const detailsKey = weatherValuesKeys.next().value;
+          const date = new Date(dt * 1000).toDateString();
+          let windSpeed = wind_speed;
+          let speedUnit = 'km/hr';
+          if (units === 'metric' || units === 'standard') {
+            windSpeed = wind_speed * 3.6;
+          } else if (units === 'imperial') {
+            speedUnit = 'mi/hr';
+          }
+          const details = (
+            <>
+              <div
+                className='more-details'
+                onClick={() => {
+                  setWeatherDetailsKey(detailsKey);
+                  setIsWeatherDetailsVisible(true);
+                }}
+              >
+                More Details...
+              </div>
+            </>
+          );
 
-      const dateKey = replaceDate(dt * 1000);
-      const isThereConflict = Object(isConflict2.get(dateKey));
+          const dateKey = replaceDate(dt * 1000);
+          const isThereConflict = Object(isConflict2.get(dateKey));
 
-      return {
-        date,
-        rain: `${rain || 0}mm`,
-        snow: `${snow || 0}cm`,
-        windSpeed: `${windSpeed.toFixed(2)} ${speedUnit}`,
-        details,
-        key: index,
-        conflict: isThereConflict.conflict,
-      };
-    }
-  );
+          return {
+            date,
+            rain: `${rain || 0}mm`,
+            snow: `${snow || 0}cm`,
+            windSpeed: `${windSpeed.toFixed(2)} ${speedUnit}`,
+            details,
+            key: index,
+            conflict: isThereConflict.conflict,
+          };
+        }
+      )
+    );
+
+    datasource.forEach((row) => console.log(row));
+
+    setRowState(datasource.forEach((row) => row.conflict));
+  }, [weatherValues]);
 
   const searchWeatherObjectsForSnow = useCallback(() => {
     const weatherData = Array.from(weatherValues.values());
@@ -78,16 +88,13 @@ const WeatherTable = () => {
   }, [searchWeatherObjectsForSnow]);
 
   const dynamicRow = useCallback(() => {
-    const rowState = datasource.forEach((row) => {
-      return row.conflict;
-    });
     if (rowState) {
       console.log('in conflict');
       return 'row-has-conflict';
     } else {
       return 'row-has-no-conflict';
     }
-  }, [weatherValues]);
+  }, [datasource]);
 
   return (
     <div className={darkMode ? 'dark-table' : 'light-table'}>
