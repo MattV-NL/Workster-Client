5b3c523439e9fcf7fc35bfafa99e6c48124f0dfb
Matt Vey <matthewvey@outlook.com>
2022-12-10 13:56:15 -0330

    removed conflict from nav bar

--- src/components/Header/TitleNav.jsx
+++ src/components/Header/TitleNav.jsx
@@ -2,7 +2,6 @@ import './titleNav.scss';
 import { Link } from 'react-router-dom';
 import { paths } from '../../constants';
 import img1 from '../../images/logo.png';
-import Compare from '../Compare/Compare';
 import LoginStatus from './LoginStatus';
 import { useContext } from 'react';
 import { UserSettingsContext } from '../../contexts/UserSettingsContext';
@@ -30,7 +29,6 @@ const TitleNav = () => {
         <Link to={paths.WORK} className='navbar-item'>
           Work
         </Link>
-        <Compare className='navbar-item' />
         <LoginStatus
           className={darkMode ? 'dark-navbar-item' : 'light-navbar-item'}
         />
